e2b9f5e6861c85de3759aaeaa25dc3851b483922
Peter Maatman <blackwolf12333@gmail.com>
2020-06-09 21:41:52 +0200

    json vec string bug

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
 use std::env;
-
-use sqlx::mysql::MySqlPool;
+use serde::{Deserialize, Serialize};
+use sqlx::mysql::{MySqlPool, MySql};
 use sqlx::FromRow;
+use sqlx::types::Json;
 
-#[derive(Debug, FromRow)]
+#[derive(Debug, Serialize, Deserialize, FromRow)]
 struct Customer {
-    customer_name: String,
+    json_column: Json<Vec<String>>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pool: MySqlPool = MySqlPool::builder()
-        .max_size(1)
-        .build(&env::var("DATABASE_URL")?)
-        .await?;
+                let pool: MySqlPool = MySqlPool::builder()
+                    .max_size(1)
+                    .build(&env::var("DATABASE_URL")? as &str)
+                    .await?;
 
-    let customer_names: Vec<Customer> = sqlx::query_as!(
-        Customer,
-        "SELECT
-            customers.name AS customer_name
+                let customer_names: Vec<Customer> = sqlx::query_as::<MySql, Customer>(
+                    "SELECT
+            json_column
         FROM
-            customers"
-    )
-    .fetch_all(&pool)
-    .await?;
+            test"
+                )
+                .fetch_all(&pool)
+                .await?;
 
-    println!("{:?}", customer_names);
-
-    Ok(())
+                println!("{:?}", customer_names);
+                Ok(())
 }
